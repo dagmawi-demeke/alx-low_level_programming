ad4a256b7be8176ae1e1e694212471527f28eb77
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-06-02 14:34:31 -0700

    fourth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+/*
+ * Write a C program that prints exactly "Programming is like building a multili*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
