f12bdcb56a658def6e6ca2d22ea3596f66b086a7
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-06-02 14:50:53 -0700

    fourth push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- * Write a C program that prints exactly "Programming is like building a
- * multilingual puzzle"
+ * main-prints the phrase "programming is like building a multilingual puzzle
+ * return 0 is exited properly, non-zero otherwise
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
