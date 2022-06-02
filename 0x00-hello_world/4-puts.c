76cd52833d7e378c56a508ba0b62d20a49a7ed1d
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-06-02 14:54:03 -0700

    fourth push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main-prints the phrase "programming is like building a multilingual puzzle
- * return 0 is exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
