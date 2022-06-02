3b3a923132fd2b05a2cf771427cf05c9fdaee06f
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-06-02 16:24:32 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
 /**
- *
  *  * main - Entry point
- *
- *   * Return:1
- *
+ *   * Return: Always 0 (Success)
  *    */
 
 int main(void)
-
 {
-
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, ch, sizeof(ch));
-
-	return (1);
-
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
