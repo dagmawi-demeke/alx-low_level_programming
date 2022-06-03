ff3e3da5c93add68526f2eec5179d57d182cd7b6
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-06-03 13:27:17 -0700

    message

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, then in uppercase.
+ * main - Prints the alphabet in lowercase, then in uppercase
  *
  * Return: 0 on success
  */
