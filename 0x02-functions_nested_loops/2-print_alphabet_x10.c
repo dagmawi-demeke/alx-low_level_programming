53d0850aebb998bd6c86707d674dd66fa313ea49
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-06-07 13:49:52 -0700

    print ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet teen times
+ * Desription: prints lower case alphabet ten times
+ * Return: returns void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i=0;
+	char c;
+
+	while (i <= 9)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		i++;
+	}
+}
