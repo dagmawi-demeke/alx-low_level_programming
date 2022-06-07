6248181d6afe89f3d635a4cb23001bb7497a8a7a
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-06-07 13:18:04 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Description: prints lowwer case alphabet
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	int i;
+
+	for (i=0; i<=z; c++)
+	{
+		_putchar(c);
+	}
+	_putchar("\n");
+}
+
