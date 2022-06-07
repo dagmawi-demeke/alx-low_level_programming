a8cb68540a72229e3d30986521066c4ddaea94ba
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-06-07 11:49:32 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,12 @@
 int main(void)
 {
 
-	char i[] = "_putchar";
+	char i[] = "_putchar\n";
 	int j;
 
-	for (j = 0; j<=8; j++)
+	for (j = 0; j <= 8; j++)
 	{
 		_putchar(i[j]);
 	}
-	_putchar('\n');
 	return (0);
 }
