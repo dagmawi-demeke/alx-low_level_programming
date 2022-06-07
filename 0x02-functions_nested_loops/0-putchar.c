183853c86526b27602edceb1dc0c3113d5fafb60
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-06-07 11:43:09 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /*
  * main - Prints _putchar
@@ -8,6 +8,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+
+	char i[] = "_putchar";
+	int j;
+
+	for (j = 0; j<=8; j++)
+	{
+		_putchar(i[j]);
+	}
+	_putchar('\n');
 	return (0);
 }
