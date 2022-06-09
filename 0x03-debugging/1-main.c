581d045d4b61ebf536c0069f440456d7dd58dd24
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-06-09 14:48:34 -0700

    first commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,10 +11,10 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 	i = 0;
-	/**
+	/*
 	 * while (i < 10)
 	 * {
-	 *	putchar(i);
+	 * 	putchar(i);
 	 * }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
