24c06a2f1f1f0fb8610b4862dca9a28396188439
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-06-16 09:43:18 -0700

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,9 +6,6 @@
  * @s2: A pointer to the second string to be compared.
  * Return: If str1 < str2, the negative difference of
  * the first unmatched characters.
- *         If s1 == s2, 0.
- *         If s1 > s2, the positive difference of
- *         the first unmatched characters.
  */
 
  int _strcmp(char *s1, char *s2)
