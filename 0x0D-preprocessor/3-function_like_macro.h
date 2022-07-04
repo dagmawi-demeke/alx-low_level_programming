b90e41979ca6172720f523e359afe0e357813f1b
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-07-04 02:40:53 -0700

    first commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-/**
+/*
  * File: 3-function_like_macro.h
  * Desc: Header file that defines a function-like macro ABS(x)
  *       that computes the absolute value of a number x.
