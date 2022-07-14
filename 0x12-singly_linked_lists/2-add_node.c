8d6af293172d87fa699205bbe417df3f5991338f
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-07-14 11:23:49 -0700

    third

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,12 +6,12 @@
  *            of a list_t list.
  * @head: A pointer to the head of the list_t list.
  * @str: The string to be added to the list_t list.
- *
  * Return: If the function fails - NULL.
  *         Otherwise - the address of the new element.
  */
 
 list_t *add_node(list_t **head, const char *str)
+
 {
 	list_t *new;
 	char *dup_str;
@@ -24,17 +24,12 @@ list_t *add_node(list_t **head, const char *str)
 	if (dup_str == NULL)
 	{
 		free(new);
-		return (NULL);
-	}
-
+		return (NULL);								}
 	for (len = 0; str[len];)
 		len++;
-
 	new->str = dup_str;
 	new->len = len;
-	new->next = *head
-	
+	new->next = *head;
 	*head = new;
-
 	return (new);
 }
