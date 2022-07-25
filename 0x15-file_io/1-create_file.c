70029e67f33ac87fa14f7919c09a604cf4540ac9
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-07-25 08:42:42 -0700

    for 1st,2nd and 3rd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,6 @@
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
- *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
@@ -28,6 +27,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	close(o);
-
 	return (1);
 }
