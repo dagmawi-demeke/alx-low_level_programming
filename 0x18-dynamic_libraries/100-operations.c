5623e12b8e3b95ec144109148d3907b2356dfd71
dagmawi <dagimagna24@gmail.com>
2022-09-05 11:47:08 -0700

    create dynamic

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,13 +1,23 @@
-#include <stdio.h>
-
-int add(int a, int *b)
+int add(int a, int b)
 {
-	int i;
-	int sum = 0;
+	return (a + b);
+}
 
-	for (i = 0; i < a; i++)
-	{
-		sum += b[i];
-	}
-	return sum;
+int sub(int a, int b)
+{
+       	return (a - b);
+}
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+int mod(int a, int b)
+{
+	return (a % b);
 }
